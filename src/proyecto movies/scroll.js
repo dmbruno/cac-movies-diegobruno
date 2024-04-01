78a149169144b5b052915c720cf33654c912f8d0
Diego Bruno <dmbruno61@gmail.com>
2024-04-01 17:32:42 -0300

    proyecto movies CAC

---
+++ src/proyecto movies/scroll.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('.contenedor');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
